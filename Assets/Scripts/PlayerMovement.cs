b2499c447541eeb19672b72d1686dc1907cf9279
JFautrat <j.fautrat@student.isartdigital.com>
2026-06-08 13:09:16 +0200

    [ADD] Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetKeyboardInput();
+    }
+
+    private void GetKeyboardInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 }
