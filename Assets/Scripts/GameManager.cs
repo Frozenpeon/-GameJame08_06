63fdfe2500385d08017d2b2d9b48f599e6583af6
Frozenpeon <thomas.verdier.sr@gmail.com>
2026-06-08 12:33:52 +0200

    [ADD] player ref in gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+
     public GameObject playerPrefab;
     public GameObject spawnPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
+        GameObject player = Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
